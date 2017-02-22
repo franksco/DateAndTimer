fbd44b728c327fedb7da33889ac8d604f3a3e7f2
franksco <franksco39@gmail.com>
2017-02-21 16:06:51 -0800

    added labels in main page with corresponding outlets on ViewController, as well as set up func to grab the date and time

--- TimerandDate/ViewController.swift
+++ TimerandDate/ViewController.swift
@@ -9,16 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateTimer() {
+        
+        
+        let timeFormatter = DateFormatter()
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .medium
+        timeFormatter.timeStyle = .medium
+        
+        
+        dateLabel.text = dateFormatter.string(from: NSDate() as Date)
+        timeLabel.text = timeFormatter.string(from: NSDate() as Date)
+        
+        
+        
+        
+    }
+    
 
 
 }
